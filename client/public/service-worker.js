4ca2577a2b5bf1ecb2fc5a594e3baa9a892d0e69
TerribleDev <1020010-TerribleDev@users.noreply.replit.com>
2025-02-15 18:02:48 +0000

    Agent query: Could you try subscribing to push notifications using the bell icon? You should see a browser prompt to allow notifications.
    Add push notification functionality using service workers.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/9dda30b6-4149-4bce-89dc-76333005952c/f599ba31-4779-4f09-8985-687ab1064b2c.jpg

---
+++ client/public/service-worker.js
@@ -0,0 +1,19 @@
+self.addEventListener('push', event => {
+  const data = event.data.json();
+  
+  event.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: data.icon,
+      badge: '/icon.png',
+      data: { url: self.location.origin }
+    })
+  );
+});
+
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow(event.notification.data.url)
+  );
+});
